32fb60bd0b00808c3ea1ffe3e779c6ac29a1806b
Maik Allgöwer <maik@llgoewer.de>
2021-07-20 18:38:22 +0200

    C6.1 - Shading with Surface Normals

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,31 @@ pub fn to_ppm<W: io::Write>(
 }
 
 fn ray_color(ray: &Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
+
+    if t > 0.0 {
+        let n = (ray.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+        0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
+    } else {
+        let unit_direction = ray.direction().unit();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
     }
-
-    let unit_direction = ray.direction().unit();
-    let t = 0.5 * (unit_direction.y() + 1.0);
-
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - center;
     let a = ray.direction().dot(ray.direction());
     let b = 2.0 * oc.dot(ray.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn main() -> std::io::Result<()> {
