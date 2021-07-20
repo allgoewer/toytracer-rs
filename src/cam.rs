f59d5c9430f784395c8f2f10c15f017c6ac9bdc9
Maik Allgöwer <maik@llgoewer.de>
2021-07-20 23:25:57 +0200

    Remove impl Default for Camera

--- src/cam.rs
+++ src/cam.rs
@@ -9,29 +9,6 @@ pub struct Camera {
     lower_left_corner: Point3,
 }
 
-impl Default for Camera {
-    fn default() -> Self {
-        let aspect_ratio = 16.0 / 9.0;
-        let viewport_height = 2.0;
-        let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
-
-        let origin = Point3::new(0.0, 0.0, 0.0);
-        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vec3::new(0.0, viewport_height, 0.0);
-
-        Self {
-            origin,
-            horizontal,
-            vertical,
-            lower_left_corner: origin
-                - horizontal / 2.0
-                - vertical / 2.0
-                - Vec3::new(0.0, 0.0, focal_length),
-        }
-    }
-}
-
 impl Camera {
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         Ray::new(
